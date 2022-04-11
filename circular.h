125ebedfdf07efe5345ac510823b1b532dcbcc65
juan_cs <juan.leandro@utec.edu.pe>
2022-04-10 20:40:35 -0500

    feat(): se agregaron nuevos metodos al CircularList Los metodos agregados son front,back,push_front,push_back,pop_front,pop_back,insert,remove

--- circular.h
+++ circular.h
@@ -4,17 +4,134 @@
 #include "list.h"
 
 // TODO: Implement all methods
+
+//IDEA: donde se pone el override? en la declaracion o en al definicion o en ambos.
 template <typename T>
-class CircularList : public List<T> {  
+class CircularList : public List<T> {
     private:
         Node<T>* head;//sentinel
         int nodes; 
     public:
         CircularList() : List<T>() { }
 
-        ~CircularList(){
-           // TODO
-        }       
+        ~CircularList();
+
+        T front() override;
+        T back() override;
+        void push_front(T data);
+        void push_back(T data);
+        T pop_front();
+        T pop_back();
+        T insert(T data, int pos);
+        bool remove(int pos);
 };
+
+template<typename T>
+CircularList<T>::~CircularList(){
+    if(head != nullptr){
+        Node<T>* temp1 = head;
+        Node<T>* temp2 = head->next;
+        while(temp1->next != head && temp2->next != head){
+            temp1 = temp2;
+            temp2 = temp2->next;
+            delete temp1;
+        }
+        delete temp2;
+        delete head;
+    }
+}
+
+template<typename T>
+T CircularList<T>:: front() override{
+    return head->next->data;
+}
+template<typename T>
+T CircularList<T>:: back() override{
+    return head->prev->data;
+}
+
+template<typename T>
+void CircularList<T>:: push_front(T data) override {
+    Node<T>* nuevo = new Node();
+    nuevo->data = data;
+
+    nuevo->next = head->next;
+    nuevo->prev = head;
+    head->next->prev = nuevo;
+    head->next = nuevo;
+
+    ++this->nodes;
+}
+
+template<typename T>
+void CircularList<T>:: push_back(T data) override {
+    Node<T>* nuevo = new Node();
+    nuevo->data = data;
+
+    nuevo->prev = head->prev;
+    nuevo->next = head;
+    head->prev->next = nuevo;
+    head->prev = nuevo;
+
+    ++this->nodes;
+}
+//IDEA: Funcion con 1 elemento, no con 0 lanzar excepcion
+template<typename T>
+void CircularList<T>:: pop_front() override {
+    Node<T>* temp = head->next->next;
+
+    temp->prev = head;
+    delete head->next;
+    head->next = temp;
+
+    --this->nodes;
+}
+//IDEA: Funcion con 1 elemento, no con 0 lanzar excepcion
+template<typename T>
+void CircularList<T>:: pop_back() override {
+    Node<T>* temp = head->prev->prev;
+
+    temp->next = head;
+    delete head->prev;
+    head->prev = temp;
+
+    --this->nodes;
+}
+
+//IDEA: lanzar una excepcion si el pos es mayor que nodes. Que quiere que devuelva?
+template<typename T>
+T CircularList<T>:: insert(T data, int pos){
+    Node<T>* temp = head;
+    Node<T>* nuevo = new Node();
+    int cont = 0;
+    nuevo->data = data;
+
+    while(cont < pos){
+        temp = temp->next;
+        ++cont;
+    }
+
+    nuevo->next = temp->next;
+    nuevo->prev = temp;
+
+    temp->next->prev = nuevo;
+    temp->next = nuevo;
+}
+
+//IDEA: excepcion si tiene 0 valores. Que quiere que retorne?
+template<typename T>
+bool CircularList<T>:: remove(int pos){
+    Node<T>* temp = head;
+    int cont = 0;
+
+    while(cont < pos){
+        temp = temp->next;
+        ++cont;
+    }
+
+    (temp->next->next)->prev = temp;
+    delete temp->next;
+    temp->next = (temp->next->next);
+}
 
 #endif
--- list.h
+++ list.h
@@ -20,7 +20,7 @@ class List {
         virtual T pop_front() = 0;
         virtual T pop_back() = 0;
         virtual T insert(T, int) = 0;
-        virtual bool remove(int) = 0;
+        virtual bool remove(int) = 0; //I'm here
         virtual T& operator[](int) = 0;//debe ser declarado en cada clase hija
         virtual bool is_empty() = 0;
         virtual int size() = 0;
